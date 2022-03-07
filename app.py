5a7e3fa571532b854d535f0acf7ca121a066044e
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2022-03-07 10:54:24 +0100

    Codigo anterior a empezar modulo bd

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ app=Flask(__name__)
 def index():
     # return "Welcome to Olivanders Shop"
 
+    items = [''] 
     data = {
             'titulo': 'Index',
             'bienvenida':'!Saludos!'
+            ''
 
             }
     return render_template('index.html', data=data) 
+
+# API REST
+api = Api(app, catch_all_404s=True)
+
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
